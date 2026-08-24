0682595d15ac7698a48a259aec13c5cec3b398a8
Chaitanya Jadhav <67154087+indicium15@users.noreply.github.com>
2026-08-24 16:07:24 +0800

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        counter = set(nums)
+        sequence_length = 0
+        # The start of the sequence is the element that does not have (n-1) in it
+        # After that, what do we do? Keep iterating through the counter while key in counter.keys()
+        # Then compare max length and update
+        for key in counter:
+            if (key-1) not in counter:
+                cur = key
+                length = 0
+                while cur in counter:
+                    cur += 1
+                    length += 1
+                sequence_length = max(sequence_length, length)
+
+        return sequence_length
+        
+
+            
+        
